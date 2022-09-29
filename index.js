1447db15dccd8e5227f75fa7f275ed2593632fca
Sambhav Jain <sambhavjain233@gmail.com>
2022-09-29 16:50:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+
+//get request
+app.get("/",function(req,res){
+    
+});
+
+
+
+
+
+
+//listen port
+app.listen(3000,function(){
+    console.log("server started at port 3000");
+});
